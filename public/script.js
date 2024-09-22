a58ae3ca99adeb2542760bf2206a07a49ff12133
Surya <suryasitit@gmail.com>
2024-09-22 17:42:52 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const navMobile = document.querySelector(".nav-buttons-mobile");
   const mainContent = document.querySelector("main");
-
   // Toggle mobile nav
   hamburger.addEventListener("click", () => {
     navMobile.classList.toggle("hidden-nav");
     toggleHamburgerIcon();
   });
-
   // Close mobile nav when clicking outside
   mainContent.addEventListener("click", () => {
     if (!navMobile.classList.contains("hidden-nav")) {
@@ -16,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
       resetHamburgerIcon();
     }
   });
-
   // Toggle hamburger icon
   function toggleHamburgerIcon() {
     const spans = hamburger.querySelectorAll("span");
@@ -24,7 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
       span.classList.toggle(`close-${index + 1}`);
     });
   }
-
   // Reset hamburger icon
   function resetHamburgerIcon() {
     const spans = hamburger.querySelectorAll("span");
